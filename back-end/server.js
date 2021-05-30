85de6e58640d4db1ce386ddba28e0bb401f2a377
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-05-30 17:08:22 -0300

    criação do servidor principal

---
+++ back-end/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+require('dotenv').config();
+
+const routes = require('./src/routes/routes');
+const invalidRoutes = require('./src/middleware/invalidRoutes')
+
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+
+//Verificação de Rotas invalidas.
+
+app.use(invalidRoutes)
+
+routes(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server listenning on http://localhost:${PORT}`));
