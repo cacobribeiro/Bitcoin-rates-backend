5c4531d8f11b5dd6dfd104dee10e255220d2213d
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-05-30 17:10:32 -0300

    criado estrutura para validar a entrada antes de chamar api

---
+++ back-end/src/middleware/requestValidation.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+
+module.exports = (schema) => {
+  return (req, res, next) => {
+    // Transformamos o objeto em um schema Joi, aceitando qualquer campo no request.
+    const reqSchema = Joi.object().keys(schema).unknown(true);
+
+    const { error, value } = reqSchema.validate(req);
+
+    if (error) {
+      const message = error.details.map((d) => d.message)[0];
+
+      console.log(message);
+
+      const reqError = { message };
+
+      return res.status(400).json(reqError);
+    }
+
+    req.body = value.body;
+    req.query = value.query;
+    req.params = value.params;
+
+    return next();
+  };
+};
