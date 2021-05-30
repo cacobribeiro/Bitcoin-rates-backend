ea80d8e5a1280b2661bfcc03197be944a44fd50f
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-05-30 17:09:08 -0300

    criado controller do login

---
+++ back-end/src/controllers/login.controller.js
@@ -0,0 +1,26 @@
+const database = require('../../database/login.json');
+const createToken = require('../utils/helpers/auth/createToken');
+
+module.exports = {
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      const data = database.find((user) => user.email === email && user.password === password);
+
+      if (!data) {
+
+        res.status(400).json({ message: 'Campos inválidos' });
+
+      }
+
+	  const token = createToken(data)
+
+      res.status(200).send({token});
+	  
+    } catch (err) {
+		
+		res.status(400).json({message: err});
+    }
+  },
+};
