54c025ed7d89f61f63783f327d39d6d19c826926
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-05-30 17:12:11 -0300

    criado controller para crypto

---
+++ back-end/src/controllers/crypto.controller.js
@@ -0,0 +1,29 @@
+const currentBTC = require('../services/currentBTC');
+const JSONTransformation = require('../services/JSONTransformation');
+
+module.exports = {
+  btc: async (req, res) => {
+    try {
+      const results = await currentBTC();
+
+      res.status(200).send(results);
+    } catch (err) {
+      res.status(400).json({ message: err });
+    }
+  },
+
+  btcPost: async (req, res) => {
+    console.log('POST');
+    try {
+      const results = await JSONTransformation(req.body);
+
+      if (results == true) {
+        res.status(200).send({
+          message: 'Valor alterado com sucesso!',
+        });
+      }
+    } catch (err) {
+      res.status(400).json({ message: err });
+    }
+  },
+};
