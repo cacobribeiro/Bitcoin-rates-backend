3e4d3c88e4b840fe82b2c1eacaccaa1bfad4158b
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-06-01 09:46:28 -0300

    fix port

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,16 @@
-const { response } = require('express');
 const fs = require('fs');
+require('dotenv').config();
 
 module.exports = (app) => {
+  const PORT = process.env.PORT || 3000;
+
   console.log('\nRodas Disponiveis:\n');
   fs.readdirSync('./src/routes/').forEach((fileName) => {
     if (fileName.includes('routes')) return;
 
     let route = '/api/' + fileName.split('.')[0];
 
-    console.log(`> http://localhost:3000${route}\n`);
+    console.log(`> http://localhost:${PORT}${route}\n`);
 
     app.use(route, require('./' + fileName));
   });
